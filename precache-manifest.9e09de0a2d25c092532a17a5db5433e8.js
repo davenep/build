f93f502f34bec6745d3b90bf5cbb65df2a70b0eb
davenp <macbook@Macbooks-MacBook-Pro.local>
2018-10-22 23:20:11 +0545

    Initial commit

---
+++ precache-manifest.9e09de0a2d25c092532a17a5db5433e8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bebacbd4fce50b1c7625",
+    "url": "/static/js/main.bebacbd4.chunk.js"
+  },
+  {
+    "revision": "f55613262100a037a913",
+    "url": "/static/js/1.f5561326.chunk.js"
+  },
+  {
+    "revision": "6cd892e66bf65942208178371275a282",
+    "url": "/index.html"
+  }
+];
